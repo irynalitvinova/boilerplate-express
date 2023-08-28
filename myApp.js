ace5be7dd69586e34edda98f5b559e54cbcb5139
Iryna Litvinova <lytvynlysko@gmail.com>
2023-08-28 19:36:46 +0300

    Create a .env file in the root of the project directory, and store the variable MESSAGE_STYLE=uppercase in it. The response object should either be {"message": "Hello json"} or {"message": "HELLO JSON"}, depending on the MESSAGE_STYLE value. At the top of the myApp.js file, add require(dotenv).config() to load the environment variables.

--- myApp.js
+++ myApp.js
@@ -1,17 +1,22 @@
+require('dotenv').config();
 let express = require('express');
 let app = express();
 app.use(express.static(__dirname + "/public"));
 // Normal usage
 app.use("/public", express.static(__dirname + "/public"));
 // Assets at the /public route
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
-app.get("/json", function(req, res) {
-  res.json({"message": "Hello json"});
+app.get("/json", function (req, res) {
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({ "message": "HELLO JSON" });
+  } else {
+    res.json({ "message": "Hello json" });
+  }
 });
 console.log("Hello World");
 
 
 
- module.exports = app;
+module.exports = app;
